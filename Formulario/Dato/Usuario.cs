846ded29a424ec300ecaa2e9abceec4398c89ce5
manolo <manolo@DESKTOP-C2ENVK5>
2024-07-18 12:25:44 +0200

    Agregar archivos de proyecto.

---
+++ Formulario/Dato/Usuario.cs
@@ -0,0 +1,34 @@
+﻿using Formulario.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formulario.Dato
+{
+    public class Usuario
+    {
+        List<UsuarioModel> lista = new List<UsuarioModel>();
+
+        /// <summary>
+        /// Guarda los usuarios
+        /// </summary>
+        /// <param name="modelo">datos del usuario</param>
+        public void Guardar(UsuarioModel modelo)
+        {
+            lista.Add(modelo);
+        }
+
+        /// <summary>
+        /// Consulta todos los usuarios
+        /// </summary>
+        /// <returns>datos de usuarios</returns>
+        public List<UsuarioModel> Consultar()
+        {
+            return lista;
+        }
+    }
+
+}
+
